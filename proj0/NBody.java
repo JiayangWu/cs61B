6a5ac60c841b42cbdca801950139d8eaf993c9a7
JiayangWu <530081999@qq.com>
2020-05-12 18:55:35 -0400

    my Nbody

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,70 +1,79 @@
 public class NBody {
-	public static double readRadius(String name) {
-		In in = new In(name);
+	public static double readRadius(String filename) {
+		In in = new In(filename);
 		int N = in.readInt();
-        double Radius = in.readDouble();
-		return Radius;
+		double R = in.readDouble();
+		return R;
 	}
 
-    public static Body[] readBodies(String name) {
-        In in = new In(name);
+	public static Body[] readBodies(String filename) {
+		In in = new In(filename);
 		int N = in.readInt();
-        double Radius = in.readDouble();
-        Body[] body_list = new Body[N];
-        for (int i = 0; i < N; i++){
-            body_list[i] = new Body(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
-        }       
-        return body_list;
-    }
-
-	// public static void main(String[] args) {
-	// 	double T = Double.parseDouble(args[0]);
-	// 	double dt = Double.parseDouble(args[1]);
-	// 	String filename = args[2];
+		double R = in.readDouble();
 		
-	// 	double radius = readRadius(filename);
-	// 	Body[] bodies = readBodies(filename);
-	// 	int N = bodies.length;
+		Body[] bodies = new Body[N];
 
-	// 	StdDraw.setScale(-radius, radius);
-	// 	StdDraw.clear();
-	// 	StdDraw.picture(0, 0, "images/starfield.jpg");
+		for (int i = 0; i < N; i++) {
+			double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            bodies[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+		}
 
-	// 	for (Body b: bodies) {
-	// 		b.draw();
-	// 	}
+		return bodies;
+	}
 
-	// 	StdDraw.enableDoubleBuffering();
+	public static void main(String[] args) {
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		
+		double radius = readRadius(filename);
+		Body[] bodies = readBodies(filename);
+		int N = bodies.length;
 
-	// 	double time = 0;
-	// 	while (time < T) {
-	// 		double[] xForces = new double[N];
-	// 		double[] yForces = new double[N];
+		StdDraw.setScale(-radius, radius);
+		StdDraw.clear();
+		StdDraw.picture(0, 0, "images/starfield.jpg");
+
+		for (Body b: bodies) {
+			b.draw();
+		}
+
+		StdDraw.enableDoubleBuffering();
+
+		double time = 0;
+		while (time < T) {
+			double[] xForces = new double[N];
+			double[] yForces = new double[N];
 			
-	// 		for (int i = 0; i < N; i++) {
-	// 			xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
-	// 			yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
-	// 		}
+			for (int i = 0; i < N; i++) {
+				xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+				yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+			}
 
-	// 		for (int i = 0; i < N; i++) {
-	// 			bodies[i].update(dt, xForces[i], yForces[i]);
-	// 		}
+			for (int i = 0; i < N; i++) {
+				bodies[i].update(dt, xForces[i], yForces[i]);
+			}
 
-	// 		StdDraw.picture(0, 0, "images/starfield.jpg");
-	// 		for (Body b: bodies) {
-	// 			b.draw();
-	// 		}
-	// 		StdDraw.show();
-	// 		StdDraw.pause(10);
-	// 		time += dt;
-	// 	}
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			for (Body b: bodies) {
+				b.draw();
+			}
+			StdDraw.show();
+			StdDraw.pause(10);
+			time += dt;
+		}
 
-	// 	StdOut.printf("%d\n", bodies.length);
-	// 	StdOut.printf("%.2e\n", radius);
-	// 	for (int i = 0; i < bodies.length; i++) {
-	// 	    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-	// 	                  bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
-	// 	                  bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
-	// 	}
-	// }
+		StdOut.printf("%d\n", bodies.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < bodies.length; i++) {
+		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		                  bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+		                  bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+		}
+	}
 }
